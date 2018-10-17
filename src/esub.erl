12af76f1246ad66195775abcf4df2486583f690b
Joe Harrison <joe@sigwinch.uk>
2018-10-18 00:47:47 +0100

    add otp command to tool

--- src/esub.erl
+++ src/esub.erl
@@ -1,5 +1,7 @@
 -module(esub).
 
+-import(esub_log, [print/1,print/2,debug/1,debug/2,fatal/2]).
+
 %% API exports
 -export([main/1]).
 
@@ -9,9 +11,23 @@
 
 %% escript Entry point
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
+    esub_log:start(),
+    print("Esub v0.0.1~n"),
+    debug("Args: ~p", [Args]),
+    [RawCmd|Args1] = Args,
+    case parse_command(RawCmd) of
+	{ok, otp} ->
+	    debug("Running command 'otp' with args ~p", [Args1]),
+	    esub_otp:main(Args1);
+	unknown ->
+	    fatal("Unknown command '~s'", [RawCmd])
+    end,
+    esub_log:stop(),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+parse_command("otp") -> {ok, otp}.
+    
